9359b3041b341f26f1d45c77982e1066b3671aee
NoviarPutra <budiawan@LAPTOP-ANHC58TL>
2022-09-06 03:19:21 +0700

    edit form and modal

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-const submit = document.getElementById("submit");
 const resultList = document.getElementById("result");
 let todolist = [];
+let key;
 
 const handlerForm = (e) => {
-  if (e.target.id == "do") {
-    todolist.push(e.target.value);
+  switch (e.target.id) {
+    case "do":
+      todolist.push(e.target.value);
+      break;
+
+    case "edit":
+      todolist[key] = e.target.value;
+      // console.log(e.target.value, key);
+      break;
+
+    default:
+      break;
   }
 };
 
-submit.addEventListener("click", (e) => {
+const submitForm = () => {
+  // console.log(todolist);
   document.getElementById("do").value = "";
-  console.log(todolist);
   const template = todolist.map((arr, index) => {
     return `
     <li class="list-group-item">
@@ -20,7 +30,7 @@ submit.addEventListener("click", (e) => {
               <button type="button" class="btn btn-outline-success btn-sm">
                 Done
               </button>
-              <button type="button" class="btn btn-outline-dark btn-sm">
+              <button id="${index}" onclick="editData(event)" value="${arr}" type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
                 Edit
               </button>
               <button type="button" class="btn btn-outline-danger btn-sm">
@@ -31,9 +41,37 @@ submit.addEventListener("click", (e) => {
         </li>`;
   });
   resultList.innerHTML = template;
-});
+};
 
-const editData = (value) => {};
+const editData = (e) => {
+  document.getElementById("edit").value = e.target.value;
+  key = e.target.id;
+  // console.log(e.target.value, key);
+};
+
+const saveEdit = () => {
+  const template = todolist.map((arr, index) => {
+    return `
+    <li class="list-group-item">
+          <div class="row">
+            <div class="col-auto me-auto">${arr}</div>
+            <div class="col-auto">
+              <button type="button" class="btn btn-outline-success btn-sm">
+                Done
+              </button>
+              <button id="${index}" onclick="editData(event)" value="${arr}" type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
+                Edit
+              </button>
+              <button type="button" class="btn btn-outline-danger btn-sm">
+                Delete
+              </button>
+            </div>
+          </div>
+        </li>`;
+  });
+  resultList.innerHTML = template;
+  // console.log(todolist);
+};
 
 const removeData = (index) => {};
 
