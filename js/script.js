61b5224867f07e3b74c6bdd0202177fe779fd5a3
NoviarPutra <noviarpb@gmail.com>
2022-09-06 05:10:29 +0700

    remove handler

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const submitForm = () => {
               <button id="${index}" onclick="editData(event)" value="${arr}" type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
                 Edit
               </button>
-              <button type="button" class="btn btn-outline-danger btn-sm">
+              <button id="${index}" onclick="removeData(event)" value="${arr}" type="button" class="btn btn-outline-danger btn-sm">
                 Delete
               </button>
             </div>
@@ -62,7 +62,7 @@ const saveEdit = () => {
               <button id="${index}" onclick="editData(event)" value="${arr}" type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
                 Edit
               </button>
-              <button type="button" class="btn btn-outline-danger btn-sm">
+              <button id="${index}" onclick="removeData(event)" value="${arr}" type="button" class="btn btn-outline-danger btn-sm">
                 Delete
               </button>
             </div>
@@ -73,6 +73,34 @@ const saveEdit = () => {
   // console.log(todolist);
 };
 
-const removeData = (index) => {};
+const removeData = (e) => {
+  let valRemove = document.getElementById("confirm");
+  valRemove = e.target.value;
+  key = e.target.id;
+  // console.log(valRemove, key);
+  if (confirm(`Confirm Delete ${valRemove}`) == true) {
+    todolist.splice(key, 1);
+  }
+  const template = todolist.map((arr, index) => {
+    return `
+    <li class="list-group-item">
+          <div class="row">
+            <div class="col-auto me-auto">${arr}</div>
+            <div class="col-auto">
+              <button type="button" class="btn btn-outline-success btn-sm">
+                Done
+              </button>
+              <button id="${index}" onclick="editData(event)" value="${arr}" type="button" class="btn btn-outline-dark btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">
+                Edit
+              </button>
+              <button id="${index}" onclick="removeData(event)" value="${arr}" type="button" class="btn btn-outline-danger btn-sm">
+                Delete
+              </button>
+            </div>
+          </div>
+        </li>`;
+  });
+  resultList.innerHTML = template;
+};
 
 const finishedData = (index) => {};
